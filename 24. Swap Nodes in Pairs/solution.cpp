2e812d088247dc6b760ba794276d7e72ed60620b
Michael Tu <michaeltu12345@gmail.com>
2023-05-16 13:08:11 -0400

    formatting

--- 24. Swap Nodes in Pairs/solution.cpp
+++ 24. Swap Nodes in Pairs/solution.cpp
@@ -11,6 +11,13 @@
  * };
  */
 class Solution {
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
 public:
     ListNode* swapPairs(ListNode* head) {
         if (head == nullptr || head->next == nullptr)
