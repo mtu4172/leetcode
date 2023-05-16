632017f1f6d73f4d32cd31cb0d6dc1e0055bb771
Michael Tu <michaeltu12345@gmail.com>
2023-05-16 13:35:39 -0400

    28. Find the Index of the First Occurrence in a String

---
+++ 28. Find the Index of the First Occurrence in a String/solution.cpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (haystack.length() < needle.length())
+            return -1;
+        
+        for (int i = 0; i < haystack.length()-needle.length()+1; i++)
+            if (haystack.substr(i, needle.length()) == needle)
+                return i; 
+        return -1;
+    }
+};
