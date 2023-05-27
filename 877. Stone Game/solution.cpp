ae2f830ccfba237119909d680b0b5847b5f14f5d
Michael Tu <michaeltu12345@gmail.com>
2023-05-27 03:28:33 -0400

    877. Stone Game

---
+++ 877. Stone Game/solution.cpp
@@ -0,0 +1,10 @@
+// Link: https://leetcode.com/problems/stone-game/
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
