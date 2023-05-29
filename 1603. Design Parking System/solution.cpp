a5ff98f102ccf90ed7e1d2ba87ba38cd82c5b886
Michael Tu <michaeltu12345@gmail.com>
2023-05-28 20:43:24 -0400

    1603. Design Parking System

---
+++ 1603. Design Parking System/solution.cpp
@@ -0,0 +1,41 @@
+// Link: https://leetcode.com/problems/design-parking-system/
+
+class ParkingSystem {
+    int b, m, s;
+public:
+    ParkingSystem(int big, int medium, int small) {
+        b = big;
+        m = medium;
+        s = small;
+    }
+    
+    bool addCar(int carType) {
+        switch (carType) {
+            case 1:
+                if (b > 0) {
+                    b--;
+                    return true;
+                }
+                return false;
+            case 2:
+                if (m > 0) {
+                        m--;
+                        return true;
+                    }
+                    return false;
+            case 3:
+                if (s > 0) {
+                    s--;
+                    return true;
+                }
+                return false;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
