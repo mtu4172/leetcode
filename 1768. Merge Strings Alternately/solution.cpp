0ce18bcc8c7c75bb19e1e6b41a07a17d045c94e9
Michael Tu <michaeltu12345@gmail.com>
2023-06-03 12:06:45 -0500

    1768. Merge Strings Alternately

---
+++ 1768. Merge Strings Alternately/solution.cpp
@@ -0,0 +1,18 @@
+// Link : https://leetcode.com/problems/merge-strings-alternately/
+
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        int len = max(word1.size(), word2.size());
+        for (int i = 0; i < len; i++) {
+            if (i < word1.size())
+                res += word1[i];
+            if (i < word2.size())
+                res += word2[i];
+        }
+        return res;
+    }
+};
