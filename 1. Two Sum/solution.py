13af950f211c47518b3a178ee15b996e0c85353a
mtu4172 <michaeltu12345@gmail.com>
2023-06-12 16:19:29 -0500

    added 1 and 2 in python

---
+++ 1. Two Sum/solution.py
@@ -0,0 +1,10 @@
+# Link: https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        inds = {}
+        for i, n in enumerate(nums):
+            if n in inds:
+                return [inds[n], i]
+            inds[target-n] = i
+        return []
