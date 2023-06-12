8a6e7601e0d1b6c3b560d58bd62a7d8eaccc89c0
mtu4172 <michaeltu12345@gmail.com>
2023-06-12 16:15:41 -0500

    Revert "1. Two Sum py"
    This reverts commit 69cb44b1238b3176434919e20363b728e51e2695.

--- 1. Two Sum/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        inds = {}
-        for i, n in enumerate(nums):
-            if n in inds:
-                return [inds[n], i]
-            inds[target-n] = i
-        return []
